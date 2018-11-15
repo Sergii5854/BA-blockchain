ad418949a566bf19c16df9f550ea5753a2676247
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-11-15 20:04:30 +0200

    add chain

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,21 +1,20 @@
+import sha256 from "sha256"
 
-    import sha256 from "sha256"
+export default class Block {
 
-    export default class Block {
-
-        constructor(index, timestamp, data, previousHash ='') {
-            this.index = index;
-            this.timestamp = timestamp;
-            this.data = data || {};
-            this.previousHash = previousHash;
-            this.hash = this.calculateHash()
-        }
-
-        calculateHash(){
-            return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data))
-        }
-
-
+    constructor(index, timestamp, data, previousHash = '') {
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data || {};
+        this.previousHash = previousHash;
+        this.hash = this.calculateHash()
     }
+
+    calculateHash() {
+        return sha256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data))
+    }
+
+
+}
 
 
---
+++ src/components/Chain.js
@@ -0,0 +1,11 @@
+import Block from "./Block"
+
+export default class Chain {
+
+    constructor() {
+        this.chain = ''
+
+    }
+
+
+}
