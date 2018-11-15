9c90bae64e42d7a8fe46509a44d2dc6275ae1334
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-11-15 18:29:00 +0200

    add new dependency sha256

--- src/components/Block.vue
+++ src/components/Block.vue
@@ -1,7 +1,15 @@
 <script>
-class Block {
-    constructor()
+    import sha256 from "sha256"
 
-}
+    export default class Block {
+
+        constructor(index, timestamp, data, previousHash ='') {
+            this.index = index;
+            this.timestamp = timestamp;
+            this.data = data;
+            this.previousHash = previousHash;
+        }
+
+    }
 
 </script>
