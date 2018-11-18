d02e1c6657600b3b30ef886d327d869940498b03
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-11-18 17:47:24 +0200

    add cain validation

--- src/models/Chain.js
+++ src/models/Chain.js
@@ -22,7 +22,20 @@ export default class Chain {
     }
 
     isValid(){
+        for(let i = 1; i<this.chain.length; i++){
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
 
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false;
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false;
+            }
+
+        }
+        return true
     }
 
 
