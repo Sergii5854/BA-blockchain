79e24c3f8565459124bd94697a5c7cce4acbc6ed
Sergii5854 <bondarenko.sergii.i@gmail.com>
2018-12-05 17:14:17 +0200

    add github pages

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # если вы публикуете по адресу https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:Sergii5854/BA-blockchain.git master:gh-pages
+git push -f git@github.com:Sergii5854/BA-blockchain master:gh-pages
 
 cd -
